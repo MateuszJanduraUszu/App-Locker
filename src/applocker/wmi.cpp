472b8fb57dee985a7d9b64b6f254f315e84f8add
MateuszJanduraUszu <Mati4321@interia.pl>
2023-07-30 14:32:25 +0200

    Add `<wmi>`

---
+++ src/applocker/wmi.cpp
@@ -0,0 +1,95 @@
+// wmi.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <applocker/service_caches.hpp>
+#include <applocker/wmi.hpp>
+#include <combaseapi.h>
+
+namespace applocker {
+    _Com_instance::_Com_instance() noexcept
+        : _Myinst(_Init()), _Mysec(_Myinst ? _Init_security() : false) {}
+
+    _Com_instance::~_Com_instance() noexcept {
+        if (_Myinst) {
+            ::CoUninitialize();
+            _Myinst = false;
+        }
+    }
+
+    bool _Com_instance::_Init() noexcept {
+        return ::CoInitializeEx(nullptr, COINIT_MULTITHREADED) == S_OK;
+    }
+
+    bool _Com_instance::_Init_security() noexcept {
+        return ::CoInitializeSecurity(nullptr, -1, nullptr, nullptr, RPC_C_AUTHN_LEVEL_DEFAULT,
+            RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE, nullptr) == S_OK;
+    }
+
+    const bool _Com_instance::_Valid() const noexcept {
+        return _Myinst && _Mysec;
+    }
+
+    _Wmi_session::_Wmi_session() noexcept
+        : _Inst(), _Locator(), _Services(), _Apartment(), _Sink(), _Stub(), _Stub_sink() {}
+
+    _Wmi_session::~_Wmi_session() noexcept {}
+
+    bool _Wmi_session::_Obtain_wmi_locator() noexcept {
+        _Locator = _Create_instance<IWbemLocator>(
+            ::CLSID_WbemLocator, CLSCTX_INPROC_SERVER, ::IID_IWbemLocator);
+        return _Locator._Valid();
+    }
+
+    bool _Wmi_session::_Connect_to_wmi() noexcept {
+        wchar_t _Namespace[] = LR"(ROOT\CIMV2)";
+        return _Locator->ConnectServer(
+            _Namespace, nullptr, nullptr, nullptr, 0, nullptr, nullptr, _Services._Address()) >= 0;
+    }
+
+    bool _Wmi_session::_Set_proxy_security() noexcept {
+        return ::CoSetProxyBlanket(_Services._Get(), RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nullptr,
+            RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE) == S_OK;
+    }
+
+    bool _Wmi_session::_Configure_event_reception() noexcept {
+        _Apartment = _Create_instance<IUnsecuredApartment>(
+            ::CLSID_UnsecuredApartment, CLSCTX_LOCAL_SERVER, ::IID_IUnsecuredApartment);
+        if (!_Apartment._Valid()) {
+            return false;
+        }
+
+        _Sink = new _Event_sink(_Service_shared_cache::_Get()._Task_event);
+        if (!_Sink._Valid()) {
+            return false;
+        }
+
+        if (_Apartment->CreateObjectStub(_Sink._Get(), _Stub._Address()) < 0) {
+            return false;
+        }
+
+        return _Stub->QueryInterface(::IID_IWbemObjectSink, _Stub_sink._Raw_address()) >= 0;
+    }
+
+    bool _Wmi_session::_Send_notification_query() noexcept {
+        wchar_t _Language[] = L"WQL";
+        wchar_t _Query[]    = L"SELECT * FROM __InstanceCreationEvent WITHIN 1 "
+                              L"WHERE TargetInstance ISA 'Win32_Process'";
+        return _Services->ExecNotificationQueryAsync(_Language, _Query, WBEM_FLAG_SEND_STATUS,
+            nullptr, _Stub_sink._Get()) >= 0;
+    }
+
+    [[nodiscard]] bool _Wmi_session::_Connect() noexcept {
+        if (!_Inst._Valid()) {
+            return false;
+        }
+
+        return _Obtain_wmi_locator() && _Connect_to_wmi() && _Set_proxy_security()
+            && _Configure_event_reception() && _Send_notification_query();
+    }
+
+    void _Wmi_session::_Terminate() noexcept {
+        _Services->CancelAsyncCall(_Stub_sink._Get());
+    }
+} // namespace applocker
---
+++ src/applocker/wmi.hpp
@@ -0,0 +1,131 @@
+// wmi.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _APPLOCKER_WMI_HPP_
+#define _APPLOCKER_WMI_HPP_
+#include <applocker/event_sink.hpp>
+#include <WbemCli.h>
+
+namespace applocker {
+    class _Com_instance {
+    public:
+        _Com_instance() noexcept;
+        ~_Com_instance() noexcept;
+
+        // checks if the COM instance is valid
+        const bool _Valid() const noexcept;
+
+    private:
+        // initializes COM
+        static bool _Init() noexcept;
+        
+        // initializes COM security
+        static bool _Init_security() noexcept;
+
+        bool _Myinst : 4; // true if the COM instance is initialized
+        bool _Mysec  : 4; // true if the COM security is initialized
+    };
+
+    template <class _Ty>
+    constexpr _Ty* _Create_instance(const CLSID& _Clsid, const CLSCTX _Ctx, const IID& _Iid) noexcept {
+        _Ty* _Result = nullptr;
+        return ::CoCreateInstance(
+            _Clsid, nullptr, _Ctx, _Iid, reinterpret_cast<void**>(&_Result)) == S_OK ? _Result : nullptr;
+    }
+
+    template <class _Ty>
+    class _Com_ptr { // wrapper for COM interfaces
+    public:
+        _Com_ptr() noexcept : _Myptr(nullptr) {}
+
+        ~_Com_ptr() noexcept {
+            if (_Myptr) {
+                _Myptr->Release();
+                _Myptr = nullptr;
+            }
+        }
+
+        _Com_ptr& operator=(_Ty* const _New_ptr) noexcept {
+            _Myptr = _New_ptr;
+            return *this;
+        }
+
+        _Ty* operator->() noexcept {
+            return _Myptr;
+        }
+
+        const _Ty* operator->() const noexcept {
+            return _Myptr;
+        }
+
+        bool _Valid() const noexcept {
+            return _Myptr != nullptr;
+        }
+
+        _Ty* _Get() noexcept {
+            return _Myptr;
+        }
+
+        const _Ty* _Get() const noexcept {
+            return _Myptr;
+        }
+
+        _Ty** _Address() noexcept {
+            return &_Myptr;
+        }
+
+        void** _Raw_address() noexcept {
+            return reinterpret_cast<void**>(&_Myptr);
+        }
+
+        _Ty* _Exchange(const _Ty* const _New_ptr) noexcept {
+            _Ty* const _Old_ptr = _Myptr;
+            _Myptr              = _New_ptr;
+            return _Old_ptr;
+        }
+
+    private:
+        _Ty* _Myptr;
+    };
+
+    class _Wmi_session { // manages WMI session
+    public:
+        _Com_instance _Inst;
+        _Com_ptr<IWbemLocator> _Locator;
+        _Com_ptr<IWbemServices> _Services;
+        _Com_ptr<IUnsecuredApartment> _Apartment;
+        _Com_ptr<_Event_sink> _Sink;
+        _Com_ptr<IUnknown> _Stub;
+        _Com_ptr<IWbemObjectSink> _Stub_sink;
+    
+        _Wmi_session() noexcept;
+        ~_Wmi_session() noexcept;
+
+        // connects to the WMI
+        [[nodiscard]] bool _Connect() noexcept;
+
+        // terminates the WMI session
+        void _Terminate() noexcept;
+    
+    private:
+        // obtains the WMI locator
+        bool _Obtain_wmi_locator() noexcept;
+
+        // connects to the WMI
+        bool _Connect_to_wmi() noexcept;
+
+        // sets security levels on the proxy
+        bool _Set_proxy_security() noexcept;
+
+        // configures the way the events are received
+        bool _Configure_event_reception() noexcept;
+
+        // sends notification query to WMI
+        bool _Send_notification_query() noexcept;
+    };
+} // namespace applocker
+
+#endif // _APPLOCKER_WMI_HPP_
