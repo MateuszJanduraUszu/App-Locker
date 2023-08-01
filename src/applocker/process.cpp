ed895c21eea27292d220995ea41a067c73c8a88b
MateuszJanduraUszu <Mati4321@interia.pl>
2023-08-01 20:13:20 +0200

    `<process>`: Implement `_Process_traits::_Get_process_list()`

--- src/applocker/process.cpp
+++ src/applocker/process.cpp
@@ -5,8 +5,44 @@
 
 #include <applocker/process.hpp>
 #include <Windows.h>
+#include <TlHelp32.h>
+#include <type_traits>
 
 namespace applocker {
+    _Toolhelp_snapshot::_Toolhelp_snapshot() noexcept : _Handle(_Create()) {}
+
+    _Toolhelp_snapshot::~_Toolhelp_snapshot() noexcept {
+        if (_Handle) {
+            ::CloseHandle(_Handle);
+            _Handle = nullptr;
+        }
+    }
+
+    [[nodiscard]] void* _Toolhelp_snapshot::_Create() noexcept {
+        return ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    }
+
+    _Process_traits::_Process_list _Process_traits::_Get_process_list() {
+        _Toolhelp_snapshot _Snapshot;
+        if (!_Snapshot._Handle) {
+            return _Process_list{};
+        }
+
+        PROCESSENTRY32W _Entry = {0};
+        _Entry.dwSize          = sizeof(PROCESSENTRY32W);
+        bool _Next             = ::Process32FirstW(_Snapshot._Handle, ::std::addressof(_Entry));
+        _Process_list _Result;
+        _Basic_data _Data;
+        while (_Next) {
+            _Data._Id              = _Entry.th32ProcessID;
+            _Data._Module_checksum = ::dbmgr::compute_checksum(_Entry.szExeFile);
+            _Result.push_back(_Data);
+            _Next = ::Process32NextW(_Snapshot._Handle, ::std::addressof(_Entry));
+        }
+
+        return _Result;
+    }
+
     void _Process_traits::_Terminate(const uint32_t _Id) noexcept {
         void* const _Handle = ::OpenProcess(PROCESS_TERMINATE, false, _Id);
         if (_Handle) { // valid process, try to terminate
--- src/applocker/process.hpp
+++ src/applocker/process.hpp
@@ -8,17 +8,37 @@
 #define _APPLOCKER_PROCESS_HPP_
 #include <cstdint>
 #include <dbmgr/checksum.hpp>
+#include <vector>
 
 namespace applocker {
+    class _Toolhelp_snapshot {
+    public:
+        void* _Handle;
+
+        _Toolhelp_snapshot() noexcept;
+        ~_Toolhelp_snapshot() noexcept;
+
+    private:
+        // creates toolhelp snapshot
+        [[nodiscard]] static void* _Create() noexcept;
+    };
+
     struct _Process_traits {
         struct _Basic_data {
             uint32_t _Id; // process ID (PID)
             ::dbmgr::checksum_t _Module_checksum; // process image file checksum
         };
 
+        using _Process_list = ::std::vector<_Basic_data>;
+
+        // returns basic data of all running processes
+        static _Process_list _Get_process_list();
+
         // terminates the specified process
         static void _Terminate(const uint32_t _Id) noexcept;
     };
+
+    using _Process_list = _Process_traits::_Process_list;
 } // namespace applocker
 
 #endif // _APPLOCKER_PROCESS_HPP_
