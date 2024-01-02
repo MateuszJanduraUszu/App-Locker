b5b9c3e29e5b53eba4d5e345b707a40e0218a746
MateuszJanduraUszu <Mati4321@interia.pl>
2024-01-02 23:22:06 +0100

    Use waitable event and SRW Lock from MJSYNC (#2)

--- src/applocker/sync.cpp
+++ src/applocker/sync.cpp
@@ -5,56 +5,20 @@
 
 #include <applocker/sync.hpp>
 
-namespace applocker {
-    shared_lock::shared_lock() noexcept : _Myimpl(SRWLOCK_INIT) {}
+namespace mjx {
+    _Sync_flag::_Sync_flag(const bool _Val) noexcept : _Myval(_Val) {}
 
-    shared_lock::~shared_lock() noexcept {}
+    _Sync_flag::~_Sync_flag() noexcept {}
 
-    void shared_lock::lock() noexcept {
-        ::AcquireSRWLockExclusive(::std::addressof(_Myimpl));
-    }
-
-    void shared_lock::lock_shared() noexcept {
-        ::AcquireSRWLockShared(::std::addressof(_Myimpl));
-    }
-
-    void shared_lock::unlock() noexcept {
-        ::ReleaseSRWLockExclusive(::std::addressof(_Myimpl));
-    }
-
-    void shared_lock::unlock_shared() noexcept {
-        ::ReleaseSRWLockShared(::std::addressof(_Myimpl));
-    }
-
-    lock_guard::lock_guard(shared_lock& _Lock) noexcept : _Mylock(_Lock) {
-        _Mylock.lock();
-    }
-
-    lock_guard::~lock_guard() noexcept {
-        _Mylock.unlock();
-    }
-
-    shared_lock_guard::shared_lock_guard(shared_lock& _Lock) noexcept : _Mylock(_Lock) {
-        _Mylock.lock_shared();
-    }
-
-    shared_lock_guard::~shared_lock_guard() noexcept {
-        _Mylock.unlock_shared();
-    }
-
-    sync_flag::sync_flag(const bool _Val) noexcept : _Myval(_Val) {}
-
-    sync_flag::~sync_flag() noexcept {}
-
-    bool sync_flag::is_set() const noexcept {
+    bool _Sync_flag::_Is_set() const noexcept {
         return _Myval.load(::std::memory_order_relaxed);
     }
 
-    void sync_flag::clear() noexcept {
+    void _Sync_flag::_Clear() noexcept {
         _Myval.store(false, ::std::memory_order_relaxed);
     }
 
-    void sync_flag::set() noexcept {
+    void _Sync_flag::_Set() noexcept {
         _Myval.store(true, ::std::memory_order_relaxed);
     }
-} // namespace applocker
+} // namespace mjx
--- src/applocker/sync.hpp
+++ src/applocker/sync.hpp
@@ -7,109 +7,65 @@
 #ifndef _APPLOCKER_SYNC_HPP_
 #define _APPLOCKER_SYNC_HPP_
 #include <atomic>
+#include <mjsync/srwlock.hpp>
 #include <type_traits>
-#include <Windows.h>
 
-namespace applocker {
-    class shared_lock {
-    public:
-        shared_lock() noexcept;
-        ~shared_lock() noexcept;
-
-        // acquires exclusive lock
-        void lock() noexcept;
-
-        // acquires shared lock
-        void lock_shared() noexcept;
-
-        // releases exclusive lock
-        void unlock() noexcept;
-
-        // releases shared lock
-        void unlock_shared() noexcept;
-
-    private:
-        SRWLOCK _Myimpl;
-    };
-
-    class lock_guard { // automatically acquires and releases exclusive lock
-    public:
-        explicit lock_guard(shared_lock& _Lock) noexcept;
-        ~lock_guard() noexcept;
-
-    private:
-        shared_lock& _Mylock;
-    };
-
-    class shared_lock_guard { // automatically acquires and releases shared lock
-    public:
-        explicit shared_lock_guard(shared_lock& _Lock) noexcept;
-        ~shared_lock_guard() noexcept;
-
-    private:
-        shared_lock& _Mylock;
-    };
-
+namespace mjx {
     template <class _Ty>
-    class locked_resource { // manages access to the shared resource
+    class _Locked_resource { // manages access to the shared resource
     public:
-        locked_resource() noexcept : _Myres() {}
+        _Locked_resource() noexcept : _Myres(), _Mylock() {}
 
-        ~locked_resource() noexcept {}
+        ~_Locked_resource() noexcept {}
 
-        locked_resource(const locked_resource&) = delete;
-        locked_resource& operator=(const locked_resource&) = delete;
+        _Locked_resource(const _Locked_resource&)            = delete;
+        _Locked_resource& operator=(const _Locked_resource&) = delete;
 
-        _Ty* operator->() noexcept {
+        _Ty& _Get() noexcept {
             lock_guard _Guard(_Mylock);
-            return ::std::addressof(_Myres);
+            return _Myres;
         }
 
-        const _Ty* operator->() const noexcept {
+        const _Ty& _Get() const noexcept {
             shared_lock_guard _Guard(_Mylock);
-            return ::std::addressof(_Myres);
+            return _Myres;
         }
 
-        _Ty* get() noexcept {
-            lock_guard _Guard(_Mylock);
-            return ::std::addressof(_Myres);
-        }
-
-        const _Ty* get() const noexcept {
-            shared_lock_guard _Guard(_Mylock);
-            return ::std::addressof(_Myres);
-        }
-
-        void assign(const _Ty& _New_val) {
+        void _Assign(const _Ty& _New_val) {
             lock_guard _Guard(_Mylock);
             _Myres = _New_val;
         }
 
+        void _Assign(_Ty&& _New_val) {
+            lock_guard _Guard(_Mylock);
+            _Myres = ::std::move(_New_val);
+        }
+
     private:
-        shared_lock _Mylock;
         _Ty _Myres;
+        shared_lock _Mylock;
     };
 
-    class sync_flag { // atomic flag for threads synchronization
+    class _Sync_flag { // atomic flag for threads synchronization
     public:
-        sync_flag(const bool _Val = false) noexcept;
-        ~sync_flag() noexcept;
+        _Sync_flag(const bool _Val = false) noexcept;
+        ~_Sync_flag() noexcept;
 
-        sync_flag(const sync_flag&) = delete;
-        sync_flag& operator=(const sync_flag&) = delete;
+        _Sync_flag(const _Sync_flag&)            = delete;
+        _Sync_flag& operator=(const _Sync_flag&) = delete;
 
         // checks if the flag is set
-        bool is_set() const noexcept;
+        bool _Is_set() const noexcept;
 
         // clears the flag
-        void clear() noexcept;
+        void _Clear() noexcept;
 
         // sets the flag
-        void set() noexcept;
+        void _Set() noexcept;
 
     private:
         ::std::atomic<bool> _Myval;
     };
-} // namespace applocker
+} // namespace mjx
 
 #endif // _APPLOCKER_SYNC_HPP_
